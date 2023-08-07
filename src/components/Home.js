e2eafa1b5de21028304d80302261bdfb7e122432
MacreenSmith <macreensmith@gmail.com>
2023-08-07 10:13:07 -0400

    Added a result Grid component for search results

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Container from './Container'
 import Searchbar from './Searchbar'
+import ResultGrid from './ResultGrid'
 export default function Home() {
   return (
     <Container>
-        <h1>Marvel Heroes!</h1>
+        <h1>Search Marvel Heroes!</h1>
         <Searchbar />
+        <ResultGrid>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+        </ResultGrid>
     </Container>
-  )
+  );
 }
 
---
+++ src/components/ResultGrid.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ResultGrid({children}) {
+  return (
+    <div className="grid">
+      {children}
+    </div>
+  )
+}
