d75f7931e32ea8305cc62b5013318918070bd8a6
MacreenSmith <macreensmith@gmail.com>
2023-08-09 09:55:55 -0400

    Chnaged color of search button, and changed useState to useRef in Searchbar.js

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { useState } from 'react'
+import { useRef } from 'react';
 import { fetchHeroes } from '../utils/fetchFromMarvel';
 
 export default function Searchbar({setter}) {
-    let input = useState("");
+    let input = useRef("");
     const handleClick = async (e) => {
         let value = input.current.value
         if (value === "") return 
